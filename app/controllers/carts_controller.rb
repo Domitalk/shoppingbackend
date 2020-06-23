0a4f271594cd11a6235ba62a7453a963f8970419
domitalk <dominic.m.chu@gmail.com>
2020-06-23 15:50:07 -0400

    cleaned up routes to only the necessary ones

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,21 +2,23 @@ class CartsController < ApplicationController
 
     def show
         @cart = Cart.find(params[:id])
-        render json: @cart.to_json
+        @cart_items = @cart.cart_items
+
+        render json: @cart_items.to_json
     end
 
     def create
-        
-    end
+        @cart = Cart.create(
+            user_id: cart_params.user_id, 
+            current_cart: true
+        )
 
-    def get_all_past_carts
-
+        render json: @cart
     end
 
     private
     def cart_params
         params.permit(:user_id)
     end 
-
 
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,18 +4,18 @@ class ItemsController < ApplicationController
         @items = Item.all
         render json: @items.to_json
     end
-    def create
-        @item = Item.create(item_params)
-        render json: @item.to_json
-    end
+    # def create
+    #     @item = Item.create(item_params)
+    #     render json: @item.to_json
+    # end
     def show
         @item = Item.find(params[:id])
         render json: @item.to_json
     end 
 
-    private 
-    def item_params
-        params.permit(:name, :price)
-    end 
+    # private 
+    # def item_params
+    #     params.permit(:name, :price)
+    # end 
     
 end 
